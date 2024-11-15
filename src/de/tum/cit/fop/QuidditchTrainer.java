d360cb69c034cb4f394bc981c893a167122a98a0
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-15 11:30:52 +0100

    Part 1 Task 4

---
+++ src/de/tum/cit/fop/QuidditchTrainer.java
@@ -0,0 +1,11 @@
+package de.tum.cit.fop;
+
+public class QuidditchTrainer extends Participant {
+    public QuidditchTrainer(String firstName, String lastName, String hogwartsID) {
+        super(firstName, lastName, hogwartsID);
+    }
+
+    public String toString() {
+        return "QuidditchTrainer{}";
+    }
+}
