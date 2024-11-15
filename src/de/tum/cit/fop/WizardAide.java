e77a58ba3f55ac177a31f9f9f854bb14a7829802
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-15 11:23:34 +0100

    Part 1 Task 3

---
+++ src/de/tum/cit/fop/WizardAide.java
@@ -0,0 +1,7 @@
+package de.tum.cit.fop;
+
+public class WizardAide extends Participant{
+    public WizardAide(String firstName, String lastName, String hogwartsID) {
+        super(firstName, lastName, hogwartsID);
+    }
+}
