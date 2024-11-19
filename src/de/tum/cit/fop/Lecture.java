fe6c94c7c6239640d796f4f91ee8f77289560089
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-19 15:16:03 +0100

    Part 1 Task 5

--- src/de/tum/cit/fop/Lecture.java
+++ src/de/tum/cit/fop/Lecture.java
@@ -6,11 +6,17 @@ public class Lecture {
     private String id;
     private String name;
     private Professor professor;
+    private List<Apprentice> apprentices;
+    private List<WizardAide> wizardsAides;
+    private List<QuidditchTrainer> quidditchTrainers;
 
-    public Lecture(String id, String name, Professor professor) {
+    public Lecture(String id, String name, Professor professor, List<Apprentice> apprentices, List<WizardAide> wizardsAides, List<QuidditchTrainer> quidditchTrainers) {
         this.id = id;
         this.name = name;
         this.professor = professor;
+        this.apprentices = apprentices;
+        this.wizardsAides = wizardsAides;
+        this.quidditchTrainers = quidditchTrainers;
     }
 
     public String getId() {
@@ -35,6 +41,30 @@ public class Lecture {
 
     public void setProfessor(Professor professor) {
         this.professor = professor;
+    }
+
+    public List<Apprentice> getApprentices() {
+        return apprentices;
+    }
+
+    public void setApprentices(List<Apprentice> apprentices) {
+        this.apprentices = apprentices;
+    }
+
+    public List<WizardAide> getWizardsAides() {
+        return wizardsAides;
+    }
+
+    public void setWizardsAides(List<WizardAide> wizardsAides) {
+        this.wizardsAides = wizardsAides;
+    }
+
+    public List<QuidditchTrainer> getQuidditchTrainers() {
+        return quidditchTrainers;
+    }
+
+    public void setQuidditchTrainers(List<QuidditchTrainer> quidditchTrainers) {
+        this.quidditchTrainers = quidditchTrainers;
     }
 
     public void addParticipant(Participant p) {
