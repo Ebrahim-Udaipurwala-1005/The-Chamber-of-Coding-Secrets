b5e274d90d41037ef2766dda0134cbcf5981f542
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-19 15:29:44 +0100

    Part 1 Task 5

--- src/de/tum/cit/fop/Apprentice.java
+++ src/de/tum/cit/fop/Apprentice.java
@@ -1,7 +1,7 @@
 package de.tum.cit.fop;
 
 public class Apprentice extends Participant{
-    public Apprentice(String firstName, String lastName, String hogwartsID, Lecture lecture) {
+    public Apprentice(String firstName, String lastName, String hogwartsID) {
         super(firstName, lastName, hogwartsID);
     }
 }
